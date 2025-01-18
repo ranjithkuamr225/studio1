6783f88a6cb3d0757ffa27d628d28cb9dc376797
ranjithkuamr225 <59768042+ranjithkuamr225@users.noreply.github.com>
2025-01-18 11:26:11 +0530

    Fix base URL for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
- 
->>>>>>> 2b835a31e776f7fd1b8ae2d1f01a1bbec422ef8e
   plugins: [react()],
   base: '/studio1/', // Replace with your repository name
   optimizeDeps: {
